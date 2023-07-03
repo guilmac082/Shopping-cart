dcbbabedc2857d245013a5bbb4b82176a9591fcf
guilmac082 <guilherme.up24@gmail.com>
2023-07-03 14:26:38 +0400

    1

--- EventFeed/EventStore.cs
+++ EventFeed/EventStore.cs
@@ -6,8 +6,7 @@ namespace ShoppingCart.EventFeed
 {
     public interface IEventStore
     {
-        Task<IEnumerable<Event>> GetEvents(long firstEventSequenceNumber,
-        long lastEventSequenceNumber);
+        Task<IEnumerable<Event>> GetEvents(long firstEventSequenceNumber, long lastEventSequenceNumber);
         Task Raise(string eventName, object content);
     }
     public class EventStore : IEventStore
